1a85be6e9b81f32eff719e64d909d7336a0a9a2b
etcetera9876 <santos@jcsfamily.com>
2025-04-15 11:11:37 -0400

    server

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,39 @@
+const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const socketIo = require('socket.io');
-const conectarDB = require('./config/db');
-const app = require('./index');
 require('dotenv').config();
+const conectarDB = require('./config/db');
 const Course = require('./models/course');
 
-const server = http.createServer(app);
+// Inicializar la aplicación Express
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Conectar a MongoDB Atlas
+conectarDB();
+
+// Configuración de rutas
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const branchRoutes = require("./routes/branchRoutes");
+const courseRoutes = require("./routes/courseRoutes");
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/branches", branchRoutes);
+app.use("/api/courses", courseRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Servidor funcionando');
+});
 
 // Configuración de Socket.IO
+const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
     origin: 'http://localhost:3000', // Cambia esto si el frontend está en otra URL
@@ -15,9 +41,6 @@ const io = socketIo(server, {
     credentials: true,
   },
 });
-
-// Conexión a MongoDB
-conectarDB();
 
 // Manejo de conexiones de Socket.IO
 io.on('connection', (socket) => {
@@ -36,22 +59,17 @@ const emitPublishedCourses = async () => {
   const currentDate = new Date(); // Fecha actual UTC
 
   try {
-    // Busca cursos cuya hora de publicación sea menor o igual a la actual
     const coursesToPublish = await Course.find({
       publicationDate: { $lte: currentDate },
     }).sort({ publicationDate: -1 }); // Ordenar por fecha de publicación, más reciente primero
 
-    // Filtra solo los cursos que no han sido emitidos antes
     const newCourses = coursesToPublish.filter(
       (course) => !scheduledCourses.has(course._id.toString())
     );
 
     if (newCourses.length > 0) {
-      // Emitir evento solo si hay nuevos cursos
       console.log('Emitiendo evento dbChange con cursos:', newCourses);
       io.emit('dbChange', coursesToPublish);
-
-      // Agregar los nuevos cursos al conjunto de cursos emitidos
       newCourses.forEach((course) => scheduledCourses.add(course._id.toString()));
     }
   } catch (error) {
